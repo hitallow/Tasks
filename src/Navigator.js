d8e03340986c90111090b3b7730030a35fd44775
Hitallo William <hitallo91@gmail.com>
2019-05-27 15:36:23 -0300

    navegação entre datas

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,19 +1,72 @@
-import { createSwitchNavigator, createAppContainer } from 'react-navigation'
+import React from 'react'
+
+import { createSwitchNavigator, createAppContainer, createDrawerNavigator } from 'react-navigation'
 
 import Agenda from './screens/Agenda'
 
 import Auth from './screens/Auth'
+import commonStyles from './commonStyles'
+
+
+const MenuRoutes = {
+    Today: {
+        name: 'Today',
+        screen: (props) => <Agenda title="Hoje" dayAhead={0} {...props} />,
+        navigationOptions: {
+            title: 'Hoje'
+        },
+    },
+    Tomorrow: {
+        name: 'Tomorrow',
+        screen: (props) => <Agenda title='Amanhã' dayAhead={1} {...props} />,
+        navigationOptions: {
+            title: 'Amanhã'
+        },
+    },
+    Week: {
+        name: 'Week',
+        screen: (props) => <Agenda title='Semana' dayAhead={7} {...props} />,
+        navigationOptions: {
+            title: 'Semana'
+        }
+    },
+    Moth: {
+        name: 'Moth',
+        screen: (props) => <Agenda title='Mês' dayAhead={30} {...props} />,
+        navigationOptions: {
+            title: 'Mês'
+        }
+    }
+}
+const MenuConfig = {
+    initialRouteName: 'Today',
+    
+    contentOptions: {
+        labelStyle: {
+            fontFamily: commonStyles.fontFamily,
+            fontWeight: 'normal',
+            fontSize: 20,
+        },
+        activeLabelStyle: {
+            color: '#080'
+        }
+    }
+}
+
+const MenuNavigator = createDrawerNavigator(MenuRoutes, MenuConfig)
+
 
 const mainRoutes = {
     Auth: {
         name: 'Auth',
-        screen:  Auth
+        screen: Auth
     },
     Home: {
         name: 'Home',
-        screen:  Agenda
+        screen: MenuNavigator
     }
 }
+
 
 const MainNavigator = createSwitchNavigator(mainRoutes, { initialRouteName: 'Auth' })
 const navigator = createAppContainer(MainNavigator)
